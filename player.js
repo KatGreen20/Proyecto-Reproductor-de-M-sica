b320b9092ff8b231865f3b445df3ccbaf506e289
KatGreen20 <cindyyazan.20@gmail.com>
2024-02-20 16:18:15 -0500

    Agregue las ultimas canciones, borre lo comentado en player.js y puse margin-left al titulo Reproducto

--- player.js
+++ player.js
@@ -44,6 +44,13 @@ class Reproductor{
             new Cancion('Bless the Broken Road', 'Rascal Flatts','3:46','Country','2004','caratula/caratula21.jpg','Audio/Bless the Broken Road.mp3'),
             new Cancion('Never enough','The Greatest Showman','3:18','Pop','2017','caratula/caratula22.jpg','Audio/never enough.mp3'),
             new Cancion('Stay with me','Punch','0:14','Pop','2016','caratula/caratula23.jpg','Audio/Stay with me.mp3'),
+            new Cancion('Do or Die', 'DPR Artic', '3:18', 'Electro-Pop', '2024', 'caratula/caratula24.jpg', 'Audio/Do or Die.mp3'),
+            new Cancion('Desire', 'Olly Alexander', '3:53', 'Electro-Pop', '2015','caratula/caratula25.jpg', 'Audio/Desire.mp3'),
+            new Cancion('Got Me Started', 'Troye Sivan', '3:16', 'Pop', '2023', 'caratula/caratula26.jpg', 'Audio/Got Me Started.mp3'),
+            new Cancion('Nerves', 'DPR IAN', '3:40', 'Pop', '2021', 'caratula/caratula27.jpg', 'Audio/Nerves.mp3'),
+            new Cancion('Rush', 'Troye Sivan', '2:52', 'Pop', '2023', 'caratula/caratula28.jpg', 'Audio/Rush.mp3'),
+            new Cancion('Touching', 'Good/Vlly','3:26', 'Electro-Pop', '2023', 'caratula/caratula29.jpg', 'Audio/touching.mp3'),
+            new Cancion('Cover Me', 'Stray Kids', '3:11', 'Pop', '2023', 'caratula/caratula30.jpg', 'Audio/Cover Me.mp3')
         ];
         this.cancionesBusqueda=[]; //array de las canciones de la busqueda
         this.cargarCanciones(); //llamo al metodo que carga el catalogo
@@ -506,148 +513,6 @@ const reproductor = new Reproductor();
 
 
 
-
-
-
-
-
-
-
-
-
-
-// class Cancion{
-//     constructor(nombre, artista, duracion, genero, anio, caratula){
-//         this.nombre= nombre;
-//         this.artista= artista;
-//         this.duracion= duracion;
-//         this.genero= genero;
-//         this.anio= anio;
-//         this.caratula= caratula;
-
-//     }
-// }
-
-// class Lista {
-//     constructor(nombre) {
-//         this.nombre = nombre;
-//         this.canciones = [];
-//     }
-
-//     agregarCancion(cancion) {
-//         this.canciones.push(cancion);
-//         console.log(`Canción "${cancion.nombre}" agregada a la lista "${this.nombre}".`);
-//     }
-
-//     eliminarCanción(cancion) {
-//         const index = this.canciones.indexOf(cancion);
-//         if (index !== -1) {
-//             this.canciones.splice(index, 1);
-//             console.log(`Canción "${cancion.nombre}" eliminada de la lista "${this.nombre}".`);
-//         } else {
-//             console.log(`La canción "${cancion.nombre}" no se encuentra en la lista "${this.nombre}".`);
-//         }
-//     }
-
-// }
-// //Clases que heredan de Lista
-// class Busqueda extends Lista {
-
-    
-    
-// }
-
-// class Favoritos extends Lista {
-    
-// }
-
-// class MiPlaylist extends Lista {
-    
-// }
-// //instanciamos lista
-// const busquedaLista = new Busqueda('Busqueda Lista');
-// const favoritosLista = new Favoritos('Favoritos Lista');
-// const miPlaylistLista = new MiPlaylist('Mi Playlist Lista');
-
-// //instanciamos cancion
-// const cancion1= new Cancion('No love', 'D&E', '3:19', 'Dance', '2020','caratula/caratula1.jpg');
-// const cancion2= new Cancion('Dark horse', 'Katy Perry', '3:35', 'Pop', '2013','caratula/caratula2.jpg');
-// const cancion3= new Cancion('Maneater', 'Nelly Furtado', '4:25', 'Electropop', '2006','caratula/caratula3.jpg');
-// const cancion4= new Cancion('Memories', 'David Guetta', '3:19', 'Technopop', '2010','caratula/caratula4.jpg');
-// const cancion5= new Cancion('Un año sin lluvia', 'Selena Gomez', '3:29', 'Pop', '2010','caratula/caratula5.jpg');
-// const cancion6= new Cancion('Escapar', 'Kudai', '4:46', 'Pop', '2004','caratula/caratula6.jpg');
-// const cancion7= new Cancion('Snowman', 'Sia', '2:45', 'Balada', '2019','caratula/caratula7.jpg');
-
-// const canciones = [cancion1, cancion2, cancion3, cancion4, cancion5, cancion6, cancion7];
-
-// function buscar(criterio) {
-//     const resultados = [];
-//     const regex = new RegExp(criterio, 'i'); 
-
-//     canciones.forEach(cancion => {
-//         if (regex.test(cancion.nombre) || regex.test(cancion.genero) || regex.test(cancion.artista)) {
-//             resultados.push(cancion);
-//         }
-//     });
-
-//     return resultados;
-// }
-
-
-// const botonBuscar = document.getElementById('botonBuscar');
-// const inputBusqueda = document.getElementById('inputBusqueda');
-
-// botonBuscar.addEventListener('click', () => {
-//     const criterio = inputBusqueda.value.trim(); 
-//     const resultados = buscar(criterio);
-
-//     const resultadosDiv = document.getElementById('resultados');
-//     resultadosDiv.innerHTML = ''; 
-
-//     resultados.forEach(cancion => {
-//         const cancionDiv = document.createElement('div');
-//         cancionDiv.innerHTML = `
-//             <p>${cancion.nombre} - ${cancion.artista}</p>
-//             <button onclick="reproducir('${cancion.nombre}')"><i class="fa-solid fa-play"></i> </button>
-//             <button onclick="agregarFavorito('${cancion.nombre}')"><i class="fa-solid fa-heart"></i></button>
-//             <button onclick="agregarAPlaylist('${cancion.nombre}')"><i class="fa-solid fa-plus"></i></button>
-//         `;
-//         resultadosDiv.appendChild(cancionDiv);
-//     });
-// });
-
-// // Función para mostrar la información de la canción
-// function mostrarInfoCancion(cancion) {
-//     const caratula = document.getElementById('caratula');
-//     const nombre = document.getElementById('nombre');
-//     const artista = document.getElementById('artista');
-//     const duracion = document.getElementById('duracion');
-//     const genero = document.getElementById('genero');
-//     const anio = document.getElementById('anio');
-
-//     caratula.src = cancion.caratula;
-//     nombre.textContent = `Nombre: ${cancion.nombre}`;
-//     artista.textContent = `Artista: ${cancion.artista}`;
-//     duracion.textContent = `Duración: ${cancion.duracion}`;
-//     genero.textContent = `Género: ${cancion.genero}`;
-//     anio.textContent = `Año: ${cancion.anio}`;
-// }
-
-// // Función para reproducir la canción
-// function reproducir(nombreCancion) {
-//     const cancion = encontrarCancionPorNombre(nombreCancion);
-//     if (cancion) {
-//         const audioPlayer = document.getElementById('audioPlayer');
-//         audioPlayer.src = `audio/${cancion.nombre}.mp3`; // Aquí asumimos que tienes una propiedad "archivo" en tu objeto Cancion
-//         audioPlayer.play();
-//         mostrarInfoCancion(cancion);
-//     }
-// }
-
-// // Función para encontrar la canción por nombre
-// function encontrarCancionPorNombre(nombreCancion) {
-//     return canciones.find(cancion => cancion.nombre === nombreCancion);
-// }
 
 
 
